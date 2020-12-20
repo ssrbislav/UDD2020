7294ec999f8ec463e610e5cd3ab1a3231311be96
Srbislav <ssrbislav@users.noreply.github.com>
2020-12-20 13:10:32 +0100

    Initial commit
    Spring boot project initialization.
    Added necessary dependencies.

---
+++ literalnoudruzenje/src/test/java/com/ftn/literalnoudruzenje/LiteralnoudruzenjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.literalnoudruzenje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteralnoudruzenjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
